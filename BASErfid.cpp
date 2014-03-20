01e4dbc95d77510764eff9898035a89467f568b6
Benjamin <Bensky@stu-34-148-171-184.champlain.edu>
2014-03-20 14:33:20 -0400

    Added notes

--- BASErfid.cpp
+++ BASErfid.cpp
@@ -19,6 +19,21 @@ void loop()
 }
 
 
-//we need code to output this to the LCD
+//we need general output code for the LCD. Once we have this, we can put whatever we want in it. lcd.print
 
+/*
+ #include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd.print("hello, world!");
+}
+
+void loop() {}
+ 
+ */
 //We need code to store the tag info in an array
+//LCD needs to display available spots in the array
+
