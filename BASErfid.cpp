811be1c19280977a62f2447dc154bd2657e9076c
sfrabe <sfrabe@gmail.com>
2014-03-20 14:41:21 -0400

    included basic lcd code in base

--- BASErfid.cpp
+++ BASErfid.cpp
@@ -1,6 +1,7 @@
 
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
 SoftwareSerial RFID(2, 3); // RX and TX
 int i;
 void setup()
@@ -22,20 +23,6 @@ void loop()
 
 
 //we need general output code for the LCD. Once we have this, we can put whatever we want in it. lcd.print
-
-/*
- #include <LiquidCrystal.h>
-
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-
-void setup()
-{
-  lcd.print("hello, world!");
-}
-
-void loop() {}
- 
- */
 //We need code to store the tag info in an array
 //LCD needs to display available spots in the array
 
