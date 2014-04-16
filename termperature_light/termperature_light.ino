a1541d98e32f78d1c7f0922d1ededc40171ab4d4
sfrabe <sfrabe@gmail.com>
2014-04-16 18:22:54 -0400

    comp arc final

--- termperature_light/termperature_light.ino
+++ termperature_light/termperature_light.ino
@@ -1,59 +1,56 @@
-//TMP36 Pin Variables
-int sensorPin = 0; //the analog pin the TMP36's Vout (sense) pin is connected to
-                        //the resolution is 10 mV / degree centigrade with a
-                        //500 mV offset to allow for negative temperatures
- 
-int photocellPin = 1;     // the cell and 10K pulldown are connected to a0
-int photocellReading;     // the analog reading from the sensor divider
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 7, 8); //Declaring pins
 
-/*
- * setup() - this function runs once when you turn your Arduino on
- * We initialize the serial connection with the computer
- */
+//Declare Variables
+int temperaturePin = 0; 
+int photocellPin = 1;    
+int photocellReading;
+
+//Initialize Serial
 void setup()
 {
-  Serial.begin(9600);  //Start the serial connection with the computer
-                       //to view the result open the serial monitor 
+  Serial.begin(9600); 
 }
- 
-void loop()                     // run over and over again
+
+//Read Temperature and Light Sensor
+void loop()
 {
- //getting the voltage reading from the temperature sensor
- int reading = analogRead(sensorPin);  
+    int reading = analogRead(temperaturePin);  
+    float voltage = reading * 5.0;
+    voltage /= 1024.0; 
  
- // converting that reading to voltage, for 3.3v arduino use 3.3
- float voltage = reading * 5.0;
- voltage /= 1024.0; 
+    float temperatureC = (voltage - 0.5) * 100 ;
+    
+    Serial.print(temperatureC); Serial.println(" degrees C");
+    
+    float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+    Serial.print(temperatureF); Serial.println(" degrees F");
  
- // print out the voltage
- Serial.print(voltage); Serial.println(" volts");
+    photocellReading = analogRead(photocellPin);  
+    Serial.print("Light Reading = ");
+    Serial.print(photocellReading);
+    
+    lcd.clear();
+    lcd.print("Temperature:");
+    lcd.print(temperatureF); lcd.print(" F");
  
- // now print out the temperature
- float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-                                               //to degrees ((voltage - 500mV) times 100)
- Serial.print(temperatureC); Serial.println(" degrees C");
- 
- // now convert to Fahrenheit
- float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
- Serial.print(temperatureF); Serial.println(" degrees F");
- 
-photocellReading = analogRead(photocellPin);  
- 
-  Serial.print("Analog reading = ");
-  Serial.print(photocellReading);     // the raw analog reading
- 
-  // We'll have a few threshholds, qualitatively determined
-  if (photocellReading < 10) {
-    Serial.println(" - Dark");
-  } else if (photocellReading < 200) {
-    Serial.println(" - Dim");
-  } else if (photocellReading < 500) {
-    Serial.println(" - Light");
-  } else if (photocellReading < 800) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }
-  
- delay(1000);                                     //waiting a second
+    if (photocellReading < 400)
+    {
+        lcd.print(" Night");
+    }
+    else if (photocellReading < 650)
+    {
+        lcd.print(" Overcast");
+    }
+    else if (photocellReading < 800)
+    {
+        lcd.print(" Partly Cloudy"); 
+    }
+    else
+    {
+        lcd.print(" Sunny");
+    }
+    
+    delay(1000);
 }
